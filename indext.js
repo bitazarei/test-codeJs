157fc3e409380af7740c0f2fde84317b554f6895
bita <bitazarei79@gmail.com>
2024-10-19 22:15:34 +0330

    add more code

--- indext.js
+++ indext.js
@@ -10,3 +10,11 @@ var job;
 va name = 'bot';
 */ 
 // consol.log(name);
+age = '23';
+job ='programmer';
+console.log(firstName + ' is age ' + age + ' whats his job? ' + job + '');
+
+alert(firstName + ' is age ' + age + ' whats his job? ' + job + '');
+
+var lastName = prompt('what is his last name?');
+console.log(firstName + ' ' + lastName)
