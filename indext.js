d93ee9b26b3361dc7009722cd725b8d3305b3647
bita <bitazarei79@gmail.com>
2024-10-25 10:14:41 +0330

    create more varibles

--- indext.js
+++ indext.js
@@ -1,3 +1,4 @@
+/*
 console.log('Hello world');
 console.log('bita');
 var firstName = 'zahra';
@@ -6,10 +7,10 @@ var age = 13;
 var fullage = true;
 console.log(fullage);
 var job;
-/*
+
 va name = 'bot';
-*/ 
-// consol.log(name);
+ 
+ consol.log(name);
 age = '23';
 job ='programmer';
 console.log(firstName + ' is age ' + age + ' whats his job? ' + job + '');
@@ -18,3 +19,15 @@ alert(firstName + ' is age ' + age + ' whats his job? ' + job + '');
 
 var lastName = prompt('what is his last name?');
 console.log(firstName + ' ' + lastName)
+*/
+var firstName = 'BITA';
+var lastName = 'Zarei';
+
+console.log('first name is : ' + firstName + ' AND last name is :' + lastName);
+
+var age , job , married ;
+age = 21;
+job = 'student';
+married = false;
+
+console.log('first name is : ' + firstName + ' AND last name is : ' + lastName +  ' , ' + age + ' years old , ' + job +' .she is married? ' + married);
