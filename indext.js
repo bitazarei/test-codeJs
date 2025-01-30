be13aa911b11e00730a82a94bde82ec487aa06e1
bita <bitazarei79@gmail.com>
2025-01-30 14:00:05 +0330

    challenge 2

--- indext.js
+++ indext.js
@@ -1,79 +1,20 @@
-// Challenge
+var x = (89+ 120+ 103)/3;
+var y = (120+ 94+ 123)/3
+var z = (97+ 134+ 105)/3;
+console.log(x,y,z);
 
-var massM , heightM , massJ , heightJ ;
-
-massM = 85;
-heightM = 1.85;
-
-massJ = 71;
-heightJ = 1.79;
-
-var BMIMark = massM / ( heightM ** 2 ) ;
-console.log(BMIMark);
-
-var BMIJohn = massJ / (heightJ ** 2);
-console.log(BMIJohn);
-if (BMIJohn > BMIMark){
-    console.log('john higher');
-}else {
-    console.log('mark higher');
+if(x > y && x > z){
+    console.log('x is winner with ' + x + ' point');
+ }
+ else if (y > x && y > z ){
+    console.log('y is winner with ' + y + ' point');
+ }
+else if (z > x && z > y) {
+    console.log('z is winner with ' +  z + ' point');
+}else if (x === z ){
+    console.log('x equal z');
+}else if (y === z){
+    console.log('y equal z');
+}else if (y === x){
+    console.log('y equal x');
 }
-// if/else statements
-var fullname = 'Bita zarei';
-var civilStatus = 'single';
-
-if (civilStatus === 'married'){
-    console.log(fullname + ' is married ');
-}else{
-    console.log(fullname + ' is so single');
-}
-// Boolean logic
-var itIsRaining = false;
-var itIsCloudy = true;
-var itIsCold = true;
-if (itIsRaining === true && itIsCloudy === false){
-    console.log('it is not cold');
-}
-else if(itIsRaining === true && itIsCloudy === true){
-    console.log('it is cold');
-}
-else{
-    console.log('it is sunny');
-}
-console.log( 'it is cloudy ? ' + ! itIsCloudy + '  ,  it is cold ? ' + ! itIsCold);
-
-if (itIsCold === false || itIsCloudy === true){
-    console.log(! true);
-    //console.log( ' it is cloudy ? ' + ! itIsCloudy + ' it is cold ? ' +  ' ' + ! itIsCold);
-}else{
-    console.log('it is sunny :)');
-}
-
-// Ternary operator
-
-itIsRaining === true ? console.log(itIsCloudy + ' ' + itIsCold)
-: console.log(! itIsCloudy + ' ' + ! itIsCold);
-
-// Switch Statements
-
-switch(itIsRaining , itIsCloudy , itIsCold){
-    case itIsRaining === true && itIsCloudy === false :
-        console.log('it is XD not cold');
-    break;
-    case itIsRaining === true && itIsCloudy === true :
-        console.log('it is cold'); 
-    break;
-    case itIsRaining === false && itIsCloudy === true :
-        console.log('it is sunny');         
-    default:
-        console.log('the weather is just like that , you know...');
-}
-
-// Truthy and falsy values
-
-var BG = '';
-if (BG) {
-    console.log('that is truthy values');
-} else {
-    console.log('that is falsy values');
-} 
