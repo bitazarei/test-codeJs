049f373f2c0d7842df23ee8d266c5f43c11d0e4a
bita <bitazarei79@gmail.com>
2025-01-31 21:22:31 +0330

    Function

--- indext.js
+++ indext.js
@@ -1,20 +1,9 @@
-var x = (89+ 120+ 103)/3;
-var y = (120+ 94+ 123)/3
-var z = (97+ 134+ 105)/3;
-console.log(x,y,z);
+// Functions
 
-if(x > y && x > z){
-    console.log('x is winner with ' + x + ' point');
- }
- else if (y > x && y > z ){
-    console.log('y is winner with ' + y + ' point');
- }
-else if (z > x && z > y) {
-    console.log('z is winner with ' +  z + ' point');
-}else if (x === z ){
-    console.log('x equal z');
-}else if (y === z){
-    console.log('y equal z');
-}else if (y === x){
-    console.log('y equal x');
+function calculateAge(year , firstName){
+    var age = 2025 - year;
+    console.log(age);
+    var retired = 65 - age;
+    console.log(firstName + ' will retire in ' + retired + ' years')
 }
+calculateAge(2006 , 'Mall');
