dc8b98d234ccda5857bec4bfed48723efae5511b
bita <bitazarei79@gmail.com>
2025-01-31 23:13:06 +0330

    Functions statement and expressions

--- indext.js
+++ indext.js
@@ -1,5 +1,5 @@
 // Functions
-
+/*
 function calculateAge(year , firstName){
     var age = 2025 - year;
     console.log(age);
@@ -7,3 +7,25 @@ function calculateAge(year , firstName){
     console.log(firstName + ' will retire in ' + retired + ' years')
 }
 calculateAge(2006 , 'Mall');
+*/ 
+// Functions statement and expressions
+
+var age = function(yearbirth , name){
+    switch(yearbirth) {
+        case '19':
+        return name + ' years old';
+        
+        case '23':
+        return name + ' years old';
+
+        case '32':
+        return name + ' years old';
+
+        case '91':
+        return name + ' years old';
+            
+    }
+}
+
+console.log(age('19','Sara' ));
+console.log(age('23','Zara' ));
