2acc281b6f0cb7560cbd302e95008801dc57a818
bita <bitazarei79@gmail.com>
2025-02-22 22:35:44 +0330

    create attribute , create document ,  create comment

--- indext.js
+++ indext.js
@@ -1,13 +1,59 @@
-// SECION 4
-// DOM , w3schools
+// adoptNode()
+/*
+const iframe = document.getElementById('myIframe');
 
-//let url0 = window.document.URL;
-let url1 = document.URL;
-//document.getElementById('Doc.window').innerHTML = url0;
-document.getElementById('Doc.window').innerHTML = url1;
-
-// HTML DOC Documet activeElement
+// body
 function myFunction() {
-    const element = document.activeElement.tagName;
-    document.getElementById("demo").innerHTML = element;
+
+  let p = document.createElement('p');
+  let txt = document.createTextNode ('my name is Bita');
+
+  p.appendChild(txt);
+  document.body.appendChild(p);
+}
+
+// characterSet
+  //The characterSet property returns the character encoding for a document.
+
+let encoding = document.characterSet;
+document.getElementById("docT").innerHTML = encoding;
+
+
+function clo() {
+  let myClo = window.open();
+  myClo.document.open();
+  myClo.document.write('<h1>BITA  ZAREI</h1>')
+  myClo.document.close();
+}
+* 
+let allcooki = document.cookie;
+document.getElementById('h1').innerHTML = allcooki;
+*/
+
+//creat attribute
+function myFunction() {
+  const attr = document.createAttribute("href");
+  attr.value = "https://www.w3schools.com";
+  
+  document.getElementById("myAnchor").setAttributeNode(attr);
   }
+
+  // Creat comments
+  var comments = document.createComment('my comment');
+  document.body.appendChild(comments);
+  document.getElementById('demo').innerHTML = 'comments are invisible';
+
+  document.getElementById('wF').innerHTML = 'Hi , how are you';
+
+  // Creat document
+
+  var fruits = ['banana', 'apple' , 'mango'];
+
+  var dfrage = document.createDocumentFragment();
+
+  for (let i in fruits) {
+    var li = document.createElement('li');
+    li.textContent = fruits[i];
+    dfrage.appendChild(li);
+  }
+document.getElementById('myList').appendChild(dfrage)
