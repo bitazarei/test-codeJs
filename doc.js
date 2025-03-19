31dfa938fd65bfbc0558433f90f03ccd8311b688
bita <bitazarei79@gmail.com>
2025-03-19 19:26:50 +0330

    dice project

--- doc.js
+++ doc.js
@@ -1,86 +1,59 @@
-var doc = document;
-console.log(doc);
+//Play dice game
+  //Dice1
+let totalSum = 0;
 
-// getElementById
-var element = document.getElementById('myList');
-console.log(element);
-console.log(typeof element);
-
-// getElementByTagName
-var TN = document.getElementsByTagName('p');
-console.log(TN);
-console.log(TN[3]);
-
-// getElementByClassName
-let CN = document.getElementsByClassName('p1');
-console.log(CN);
-
-// querySelector
-var QS = document.querySelector('p')
-console.log(QS);
-
-// querySelectorAll
-var QSA = document.querySelectorAll('.p1')
-console.log(QSA);
-
-var showL = document.querySelector('.myList')
-console.log(
-    typeof showL ,
-    showL.parentNode.nodeName , 
-    showL.firstElementChild ,
-    showL.lastElementChild ,
-    showL.childElementCount
-    );
-
-console.log(showL.innerHTML);
-
-// attribute add , remove , call 
-
-  // call
-var callP = document.getElementById('myp1');
-console.log(callP.id);
-
-var callP2 = document.getElementById('myp1');
-console.log(callP2.className);
-
-var callP2 = document.getElementById('myp1');
-console.log(callP2.title);
-
-  // getAttribute | call another
-var callAnother = document.getElementById('myp1');
-console.log(callAnother.getAttribute('id'));
-
-  // setAttribute | add
-var add = document.getElementById('myp1');
-//console.log(add.setAttribute('class','show')); //correct
-add.className = 'myClass';    //correct
-
-  // removeAttribute | remove
-var remove = document.getElementById('myp1');
-console.log(remove.removeAttribute('title'));
-
-
-// Floor method
-console.log(Math.floor(1.2));
-console.log(Math.floor(-3.2));
-
-// Random method
-//var RM = Math.random()*11
-//console.log(RM);
-
-// Bach tick
-var firstName = 'Bita';
-console.log(`my name is ${firstName}`);
-
-// Random Color
-function randNum(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+function dice1() {
+  var Dice = Math.floor(Math.random() * 6) + 1;
+  return Dice ; 
 }
 
-function randColor() {
-  return `rgb(${randNum(20 , 120)}, ${randNum(20 , 130)} , ${randNum(20 , 130)})`;
+function dicePlay1() {
+  const roll = dice1(); // Roll the dice
+  totalSum += roll;
+  document.getElementById('dice').innerHTML = roll;
+  checkPoints();
 }
 
-function changeColor() {
-document.getElementById("demo").style.backgroundColor = randColor();
+function sumDice1() {
+  document.getElementById('sumDice').innerHTML = totalSum;
 }
+
+  // Dice 2
+let TotalSum = 0;
+
+function dice2() {
+  var Dice = Math.floor(Math.random() * 6) + 1;
+  return Dice ; 
+}
+
+function dicePlay2() {
+  const roll = dice2(); // Roll the dice
+  TotalSum += roll;
+  document.getElementById('dice2').innerHTML = roll;
+  checkPoints();
+} 
+
+function sumDice2() {
+  document.getElementById('sumDice2').innerHTML = TotalSum;
+}
+
+function checkPoints() {
+  if (totalSum >= 50){
+    document.getElementById('textW').innerHTML = 'Player 1 won';
+    document.getElementById('gameOver').innerHTML = 'Game over';
+    document.getElementById('gemeAgane').innerHTML = 'Game agane ?';
+    var on = document.getElementById('gemeAgane');
+    on.onclick = function onC() {
+      location.reload();
+    }
+   }
+  else if (TotalSum >= 50){
+    document.getElementById('textW').innerHTML = 'Player 2 won';
+    document.getElementById('gameOver').innerHTML = 'Game over';
+  }
+}
+// function gameA() {
+//             document.getElementById('gemeAgane').innerHTML = location.reload();
+
+//      }
+// document.getElementById('textW').style = '.textWinner';
