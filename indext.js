a4fa81a65fb44d82019bbe745e55be366cdb5c36
bita <bitazarei79@gmail.com>
2025-08-08 15:59:10 +0430

    creat new mini peroject

--- indext.js
+++ indext.js
@@ -1,25 +1,258 @@
+const boxA = document.querySelector('.a');
+const boxB = document.querySelector('.b');
+const boxC = document.querySelector('.c');
+const boxD = document.querySelector('.d');
+const boxE = document.querySelector('.e');
+const boxF = document.querySelector('.f');
+const boxG = document.querySelector('.g');
+const boxH = document.querySelector('.h');
+const boxI = document.querySelector('.i');
+const boxJ = document.querySelector('.j');
+const boxK = document.querySelector('.k');
+const boxL = document.querySelector('.l');
+const boxM = document.querySelector('.m');
+const boxN = document.querySelector('.n');
+const boxO = document.querySelector('.o');
+const boxP = document.querySelector('.p');
+const boxQ = document.querySelector('.q');
+const boxR = document.querySelector('.r');
+const boxS = document.querySelector('.s');
+const boxT = document.querySelector('.t');
+const boxU = document.querySelector('.u');
+const boxV = document.querySelector('.v');
+const boxW = document.querySelector('.w');
+const boxX = document.querySelector('.x');
+const boxY = document.querySelector('.y');
+const boxZ = document.querySelector('.z');
+//const showClick = document.querySelector('.div');
 const input = document.querySelector('.myInput');
-const p = document.querySelector('.myP');
-const body = document.querySelector('body');
-// input.addEventListener('keydown', (event) => {
-//     p.innerHTML += `key : ${event.key} </br>`;
-//     p.innerHTML += `event type : ${event.type}</br>`;
+//const p = document.querySelector('.myP');
+//const p1 = document.querySelectorAll('.pp');
+//const container = document.querySelector('.container');
 
-    // p.innerHTML += `targrt : ${event.target.nodeName}</br>`;
-    // p.innerHTML += `event code : ${event.code}</br>`;
-    // p.innerHTML += ` alt : ${event.altKey}</br>`;
-    // p.innerHTML += ` ctrl : ${event.ctrlKey}</br>`;
-    // p.innerHTML += ` shift : ${event.shiftKey}</br>`;
-    // p.innerHTML += ` repeat : ${event.repeat}</br>`;
+input.addEventListener('keydown', (x) => {
+    let key = x.key;
+    switch (key) {
+        case 'a':
+    case 'A':
+        boxA.classList.add('action');
+        break;
+    case 'b':
+    case 'B':
+        boxB.classList.add('action');
+        break;
+    case 'c':
+    case 'C':
+        boxC.classList.add('action');
+        break;
+    case 'd':
+    case 'D':
+        boxD.classList.add('action');
+        break;
+    case 'e':
+    case 'E':
+        boxE.classList.add('action');
+        break;
+    case 'f':
+    case 'F':
+        boxF.classList.add('action');
+        break;
+    case 'g':
+    case 'G':
+        boxG.classList.add('action');
+        break;
+    case 'h':
+    case 'H':
+        boxH.classList.add('action');
+        break;
+    case 'i':
+    case 'I':
+        boxI.classList.add('action');
+        break;
+    case 'j':
+    case 'J':
+        boxJ.classList.add('action');
+        break;
+    case 'k':
+    case 'K':
+        boxK.classList.add('action');
+        break;
+    case 'l':
+    case 'L':
+        boxL.classList.add('action');
+        break;
+    case 'm':
+    case 'M':
+        boxM.classList.add('action');
+        break;
+    case 'n':
+    case 'N':
+        boxN.classList.add('action');
+        break;
+    case 'o':
+    case 'O':
+        boxO.classList.add('action');
+        break;
+    case 'p':
+    case 'P':
+        boxP.classList.add('action');
+        break;
+    case 'q':
+    case 'Q':
+        boxQ.classList.add('action');
+        break;
+    case 'r':
+    case 'R':
+        boxR.classList.add('action');
+        break;
+    case 's':
+    case 'S':
+        boxS.classList.add('action');
+        break;
+    case 't':
+    case 'T':
+        boxT.classList.add('action');
+        break;
+    case 'u':
+    case 'U':
+        boxU.classList.add('action');
+        break;
+    case 'v':
+    case 'V':
+        boxV.classList.add('action');
+        break;
+    case 'w':
+    case 'W':
+        boxW.classList.add('action');
+        break;
+    case 'x':
+    case 'X':
+        boxX.classList.add('action');
+        break;
+    case 'y':
+    case 'Y':
+        boxY.classList.add('action');
+        break;
+    case 'z':
+    case 'Z':
+        boxZ.classList.add('action');
+        break;
+
+        default:
+            break;
+    }
+})
+input.addEventListener('keyup', (y) => {
+    let key = y.key;
+    switch (key) {
+        case 'a':
+        case 'A':    
+            boxA.classList.replace('action','pass');
+            break;
+      
+    case 'b':
+    case 'B':
+        boxB.classList.replace('action','pass');
+        break;
+    case 'c':
+    case 'C':
+        boxC.classList.replace('action','pass');
+        break;
+    case 'd':
+    case 'D':
+        boxD.classList.replace('action','pass');
+        break;
+    case 'e':
+    case 'E':
+        boxE.classList.replace('action','pass');
+        break;
+    case 'f':
+    case 'F':
+        boxF.classList.replace('action','pass');
+        break;
+    case 'g':
+    case 'G':
+        boxG.classList.replace('action','pass');
+        break;
+    case 'h':
+    case 'H':
+        boxH.classList.replace('action','pass');
+        break;
+    case 'i':
+    case 'I':
+        boxI.classList.replace('action','pass');
+        break;
+    case 'j':
+    case 'J':
+        boxJ.classList.replace('action','pass');
+        break;
+    case 'k':
+    case 'K':
+        boxK.classList.replace('action','pass');
+        break;
+    case 'l':
+    case 'L':
+        boxL.classList.replace('action','pass');
+        break;
+    case 'm':
+    case 'M':
+        boxM.classList.replace('action','pass');
+        break;
+    case 'n':
+    case 'N':
+        boxN.classList.replace('action','pass');
+        break;
+    case 'o':
+    case 'O':
+        boxO.classList.replace('action','pass');
+        break;
+    case 'p':
+    case 'P':
+        boxP.classList.replace('action','pass');
+        break;
+    case 'q':
+    case 'Q':
+        boxQ.classList.replace('action','pass');
+        break;
+    case 'r':
+    case 'R':
+        boxR.classList.replace('action','pass');
+        break;
+    case 's':
+    case 'S':
+        boxS.classList.replace('action','pass');
+        break;
+    case 't':
+    case 'T':
+        boxT.classList.replace('action','pass');
+        break;
+    case 'u':
+    case 'U':
+        boxU.classList.replace('action','pass');
+        break;
+    case 'v':
+    case 'V':
+        boxV.classList.replace('action','pass');
+        break;
+    case 'w':
+    case 'W':
+        boxW.classList.replace('action','pass');
+        break;
+    case 'x':
+    case 'X':
+        boxX.classList.replace('action','pass');
+        break;
+    case 'y':
+    case 'Y':
+        boxY.classList.replace('action','pass');
+        break;
+    case 'z':
+    case 'Z':
+        boxZ.classList.replace('action','pass');
+        break;
+        default:
+            break;
+    }
+})
 
 
-    input.addEventListener('keydown', (event) => {
-    //p.innerHTML += `key : ${event.key.a} </br>`;
-    let key = event.key;
-      let ctrl = event.ctrlKey;
-      p.innerHTML += key;
-        if (key === 'a' & ctrl === true ) {
-         document.body.style.backgroundColor = 'salmon';
-        }
-       })
-        
